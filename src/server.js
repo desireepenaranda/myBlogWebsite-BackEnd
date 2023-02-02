ce1b9b3107903c372a244f84e73c8d4ac5c668b8
desireepenaranda <penad@seas.upenn.edu>
2023-02-02 16:12:41 -0500

    added the functionality for making a comment and needing to be logged in to edit an article and have email name

--- src/server.js
+++ src/server.js
@@ -1,13 +1,42 @@
+import fs from "fs";
+import admin from "firebase-admin";
 import express from "express";
 import { db, connectToDb } from "./db.js";
+
+// loading credentials json package
+const credentials = JSON.parse(fs.readFileSync("./credentials.json"));
+//
+admin.initializeApp({ credential: admin.credential.cert(credentials) });
 
 const app = express();
 app.use(express.json());
 
+// load user from the auth token included in the request header
+app.use(async (req, res, next) => {
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    try {
+      const user = await admin.auth().verifyIdToken(authtoken);
+    } catch (e) {
+      return res.sendStatus(400);
+    }
+  }
+  req.user = req.user || {};
+  next();
+});
+
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
+  const { uid } = req.user;
   const article = await db.collection("articles").findOne({ name });
   if (article) {
+    // check to see if user has already upvoted the article
+    const upvoteIds = article.upvoteIds || [];
+    if (uid) {
+      article.canUpVote = !upvoteIds.includes(uid);
+    }
+    // article.canUpVote = uid && !upvoteIds.includes(uid);
     res.json(article);
   } else {
     res.sendStatus(404);
@@ -15,9 +44,7 @@ app.get("/api/articles/:name", async (req, res) => {
 });
 
 app.get("/api/articles", async (req, res) => {
-  console.log("made it into the articles get function");
   const articles = await db.collection("articles").find({});
-  console.log(articles);
   if (articles) {
     res.json(articles);
   } else {
@@ -35,20 +62,37 @@ app.put("/api/articles/", async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    // error meaning user not able to access end points
+    res.sendStatus(401);
+  }
+});
+
 app.put("/api/articles/:name/upvote", async (req, res) => {
-  console.log("wihtihn the upvote API request ");
   const { name } = req.params;
-
-  await db.collection("articles").updateOne(
-    { name },
-    {
-      $inc: { upvotes: 1 },
-    }
-  );
+  const { uid } = req.user;
   const article = await db.collection("articles").findOne({ name });
-
   if (article) {
-    res.send(article);
+    const upvoteIds = article.upvoteIds || [];
+    const canUpVote = uid && !upvoteIds.includes(uid);
+
+    if (canUpVote) {
+      if (upvoteIds === []) {
+        upvoteIds.push(uid);
+      }
+      await db.collection("articles").updateOne(
+        { name },
+        {
+          $inc: { upvotes: 1 },
+          $push: { upvoteIds: uid },
+        }
+      );
+    }
+    const updatedArticle = await db.collection("articles").findOne({ name });
+    res.json(updatedArticle);
   } else {
     res.send("That article doesn't exist!");
   }
@@ -57,10 +101,10 @@ app.put("/api/articles/:name/upvote", async (req, res) => {
 app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
   const { postedBy, text } = req.body;
-
+  const email = req.user.email;
   await db
     .collection("articles")
-    .updateOne({ name }, { $push: { comments: { postedBy, text } } });
+    .updateOne({ name }, { $push: { comments: { postedBy: postedBy, text } } });
   const article = await db.collection("articles").findOne({ name });
   if (article) {
     res.json(article);
